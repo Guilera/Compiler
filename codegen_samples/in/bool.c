9d40c1c052419bf5a049602f63310fa235095f35
Guilera <brunoguilera@dcc.ufba.br>
2018-07-31 17:38:33 -0300

    fix bool sample

--- codegen_samples/in/bool.c
+++ codegen_samples/in/bool.c
@@ -5,7 +5,7 @@ void main(void){
   println(0 + 0);
   println(1 + 3 * 5);
   println((3 + 3) * 10 + (4 - 0) * 100);
-  println(1 < 0);
+  println(3 * (20 + (1 - 6) + (2 + 2)*(0 - 1)));
   println(59);
   println(1-2);
   println(49 - 37 + 5);
